e58d5549b15e495f234c57e63fa55cef0b050b09
Lister Cai <2295038285@qq.com>
2018-08-04 10:45:17 +0800

    修改About

--- app/src/main/java/com/weiwobang/paotui/activity/AboutActivity.java
+++ app/src/main/java/com/weiwobang/paotui/activity/AboutActivity.java
@@ -2,16 +2,34 @@ package com.weiwobang.paotui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.weiwobang.paotui.R;
 import com.weiwobang.paotui.tools.ActManager;
 
-public class AboutActivity extends AppCompatActivity {
 
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AboutActivity extends AppCompatActivity {
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wwb_activity_about);
         ActManager.getAppManager().addActivity(this);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
